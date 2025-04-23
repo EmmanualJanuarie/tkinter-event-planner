ab0dd0a96e1d39018dea6aa15ab0b4ef396ff334
Emmanual <127601685+EmmanualJanuarie@users.noreply.github.com>
2025-04-23 13:16:34 +0200

    updated signup_Auth.py

---
+++ backend/authentication/signup_Auth.py
@@ -0,0 +1,5 @@
+from tkinter import messagebox
+
+# Add sign up authentication code here
+# if user already exists display a error message
+#if user is successfulli registered display a message and direct user to login GUI
