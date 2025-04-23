5d145afc18b0637ae18f8b80cc70d3f0517964c3
Emmanual <127601685+EmmanualJanuarie@users.noreply.github.com>
2025-04-23 13:16:18 +0200

    updated login_Auth.py

---
+++ backend/authentication/login_Auth.py
@@ -0,0 +1,6 @@
+from tkinter import messagebox
+
+# aDD LOGIN AUTHENTICATION CODE HERE
+# if user does not exist display an error message
+# if user enters worng password or email display a message
+# is user enters the correct password and email direct user after loading attempt to the content
